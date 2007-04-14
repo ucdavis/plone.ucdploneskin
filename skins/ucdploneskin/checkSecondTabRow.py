faeda572baf6f9cbdbe94daefd5d6eaf4b592468
Charles McLaughlin <cmclaugh@ucdavis.edu>
2007-04-14 18:46:31 +0000

    see HISTORY.TXT

--- skins/ucdploneskin/checkSecondTabRow.py
+++ skins/ucdploneskin/checkSecondTabRow.py
@@ -3,8 +3,29 @@
 
 # called in ploneCustom.css.dtml to decide how to style tabs
 
-if context.portal_properties.ucdploneskin_properties.secondTabRow:
-  return 1
+from Products.CMFCore.utils import getToolByName
 
-else:
-  return 0
+# get the portal object
+container = context.aq_inner.aq_parent
+
+atool = getToolByName(context, 'portal_actions')
+actions = atool.listFilteredActionsFor(container)
+portal_tabs_view = container.restrictedTraverse('@@portal_tabs_view')
+
+tabs = portal_tabs_view.topLevelTabs(actions)
+
+secondTabRow = context.portal_properties.ucdploneskin_properties.secondTabRow
+secondTabRowAr = secondTabRow.split(',')
+
+# we have stuff in the second row
+
+if secondTabRow:
+
+  # loop through the list of portal tabs, if any one tab isn't in the second
+  # row, then we we have two rows
+
+  for tab in tabs:
+    if tab['id'] not in secondTabRowAr:
+      return 1
+
+return 0
