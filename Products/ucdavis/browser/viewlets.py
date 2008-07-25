31f9bd3f04237a07fd5174a5319a80d976d82340
cmcstone <cbeck@ucdavis.edu>
2008-07-25 17:31:36 +0000

    add sections viewlet

--- Products/ucdavis/browser/viewlets.py
+++ Products/ucdavis/browser/viewlets.py
@@ -1,5 +1,7 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getMultiAdapter
+
 
 class LogoViewlet(ViewletBase):
     index = ViewPageTemplateFile('logo.pt')
@@ -14,3 +16,23 @@ class LogoViewlet(ViewletBase):
         self.logo_tag = portal.restrictedTraverse(logoName).tag()
 
         self.portal_title = self.portal_state.portal_title()
+
+
+class GlobalSectionsViewlet(ViewletBase):
+    index = ViewPageTemplateFile('sections.pt')
+
+    def update(self):
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        actions = context_state.actions()
+        portal_tabs_view = getMultiAdapter((self.context, self.request),
+                                           name='portal_tabs_view')
+        self.portal_tabs = portal_tabs_view.topLevelTabs(actions=actions)
+
+        selectedTabs = self.context.restrictedTraverse('selectedTabs')
+        self.selected_tabs = selectedTabs('index_html',
+                                          self.context,
+                                          self.portal_tabs)
+        self.selected_portal_tab = self.selected_tabs['portal']
+
+
