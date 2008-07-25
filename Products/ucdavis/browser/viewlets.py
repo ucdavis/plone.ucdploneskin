0a9423f5fdfd14bed1fbcf587f0c36e65d6effdd
cmcstone <cbeck@ucdavis.edu>
2008-07-25 17:16:35 +0000

    Add customizable logo viewlet

--- Products/ucdavis/browser/viewlets.py
+++ Products/ucdavis/browser/viewlets.py
@@ -1,3 +1,16 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
+class LogoViewlet(ViewletBase):
+    index = ViewPageTemplateFile('logo.pt')
+
+    def update(self):
+        super(LogoViewlet, self).update()
+
+        self.navigation_root_url = self.portal_state.navigation_root_url()
+
+        portal = self.portal_state.portal()
+        logoName = portal.restrictedTraverse('base_properties').logoName
+        self.logo_tag = portal.restrictedTraverse(logoName).tag()
+
+        self.portal_title = self.portal_state.portal_title()
