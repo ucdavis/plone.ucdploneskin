efee13a8585d079b16503352c73772fe26a96b54
cmcstone <cbeck@ucdavis.edu>
2008-07-25 22:30:38 +0000

    Add ucdskin.portalbarheader ViewManager and put the path and personal bars in it

--- Products/ucdavis/browser/interfaces.py
+++ Products/ucdavis/browser/interfaces.py
@@ -1,5 +1,11 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
+    """
+
+class IPortalBarHeader(IViewletManager):
+    """Marker interface for view manager above content
+       and right column.
     """
