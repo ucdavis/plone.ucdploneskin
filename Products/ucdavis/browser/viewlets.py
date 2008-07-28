16ac65c8cbe57a75a641f80f041dc9d75260b043
smcmahon <steve@dcn.org>
2008-07-28 18:09:04 +0000

    Double row global sections now enabled. Still needs CSS support.

--- Products/ucdavis/browser/viewlets.py
+++ Products/ucdavis/browser/viewlets.py
@@ -30,7 +30,7 @@ class PathBarViewlet(ViewletBase):
         breadcrumbs_view = getMultiAdapter((self.context, self.request),
                                            name='breadcrumbs_view')
         self.breadcrumbs = breadcrumbs_view.breadcrumbs()
-
+        
 
 class GlobalSectionsViewlet(ViewletBase):
     index = ViewPageTemplateFile('sections.pt')
@@ -48,5 +48,25 @@ class GlobalSectionsViewlet(ViewletBase):
                                           self.context,
                                           self.portal_tabs)
         self.selected_portal_tab = self.selected_tabs['portal']
+
+        # support for double-row globalsections with second row specification
+        # drawn from ucdploneskin_properties.secondTabRow
+        ucdprops = getattr(self.context, 'ucdploneskin_properties', None)
+        if ucdprops is not None:
+          secondTabRow = getattr(ucdprops, 'secondTabRow', '').split(',')
+          row1 = []
+          row2 = []
+          for tab in self.portal_tabs:
+            if tab['id'] in secondTabRow:
+              row2.append(tab)
+            else:
+              row1.append(tab)
+          self.portal_tab_rows = []
+          self.portal_tab_styles = ['portal-globalnav-2']
+          if len(row1):
+            self.portal_tab_rows.append(row1)
+          if len(row2):
+            self.portal_tab_rows.append(row2)
+            self.portal_tab_styles = ['portal-globalnav-1','portal-globalnav-2']
 
 
